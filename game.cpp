b3a7b76f10d124bd9298b1071d5b3c415111a51a
Kraig Taylor <tay15028@byui.edu>
2020-10-02 21:27:45 -0600

    added class structure for Game class

---
+++ game.cpp
@@ -0,0 +1,19 @@
+#include "game.h"
+
+// initialize the room and start the main game loop
+void Game::startGame()
+{
+
+}
+
+// handle the text input from the player
+void Game::handleCommand(std::string command, std::string target = std::string())
+{
+
+}
+
+// The main game loop
+void Game::playGame()
+{
+
+}
---
+++ game.h
@@ -0,0 +1,26 @@
+/*****************************************************
+ * class: Game
+ * The class that encapsulates the game logic/loop
+ * ***************************************************/
+#ifndef GAME_H
+#define GAME_H
+
+#include <vector>
+#include <iostream>
+#include "item.h"
+
+class Game
+{
+    private:
+    std::vector<GameObject> environment;
+    std::vector<Item> items;
+    std::string command;
+    void handleCommand(std::string command, std::string target = std::string());
+    void playGame();
+
+    public:
+    Game();
+    void startGame();
+};
+
+#endif
